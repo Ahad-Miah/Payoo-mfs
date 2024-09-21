70e8298426530e07786bff2b80f137096c0ebe33
Ahad-Miah <ahadahmedcc@gmail.com>
2024-09-21 20:13:47 +0600

    login page done

---
+++ js/login.js
@@ -0,0 +1,15 @@
+document.getElementById('button').addEventListener('click',function(event){
+   event.preventDefault();
+   const phoneNumber = document.getElementById('phone-number').value;
+   const pinNumber = document.getElementById('pin-number').value;
+   
+   if(phoneNumber==="01745007573" && pinNumber==="1234"){
+    window.location.href ="./home.html";
+
+   }
+   else{
+    alert("Wrong phone number or pin");
+   }
+
+
+})
